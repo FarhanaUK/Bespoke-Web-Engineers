b8f6915daa15a8184b359b3c04a7cee7ca0e0165
Farhana Aktar <farhanaaktar@live.co.uk>
2025-04-15 20:00:24 +0100

    WIP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,20 +12,20 @@ function Hero() {
 
       <div className=" relative w-full h-[400px] mt-8 ">
         <img
-          src="/public/S1.svg"
+          src="/S1.svg"
           alt="Background"
-          className="absolute right-0 opacity-85 top-0 h-full object-contain w-[55%]"
+          className="absolute right-0 top-0 h-full object-contain w-[55%]"
         />
-        <div className="absolute right-0 top-2/5 transform -translate-y-1/2 transform -translate-x-46 w-[25%] border bg-gray-900 rounded-xl opacity-85 text-white p-4 flex flex-col items-end justify-center text-black text-left border px-4">
+        <div className="absolute right-0 top-2/5 transform -translate-y-1/2 transform -translate-x-46 w-[25%] border bg-gray-900 rounded-xl opacity-75 text-white p-4 flex flex-col items-end justify-center text-left border px-4">
 
           <h2 className="text-2xl font-bold">We Build Beautiful Websites</h2>
-          <p className="mt-2 ">Custom coded to perfectly match <br/>your brand identity and business objectives.<br/> Every line of code is carefully<br/> crafted for optimal performance<br/> and user experience.</p>
+          <p className="mt-2 ">Custom coded to perfectly match your brand identity and business objectives.<br/>Every line of code is carefully crafted for optimal performance and user experience.</p>
         </div>
-        
+      
       
           <img src="S2.svg"
            alt="Background"
-           className="absolute left-35 top-0 h-full object-contain w-[30%] mt-20 opacity-85"/>
+           className="absolute left-35 top-0 h-full object-contain w-[30%] mt-20 "/>
           <div className="absolute flex flex-col items-start justify-center border bg-gray-900 rounded-xl opacity-75 text-white p-4 text-left px-4 transform translate-x-65 transform translate-y-60">
           <h2 className="text-2xl font-bold text-white p-2">Premium Web Development <br/>Services</h2>
           <p className="mt-2 ">From responsive design to e-commerce solutions, <br/>we create digital experiences that engage visitors<br/> and convert them into customers.</p>
