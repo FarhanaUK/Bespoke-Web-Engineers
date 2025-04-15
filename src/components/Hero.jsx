042d28802086228dfce8cd1ca5702d490bde05c1
Farhana Aktar <farhanaaktar@live.co.uk>
2025-04-15 18:07:11 +0100

    Hero in progress

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,27 +8,28 @@ function Hero() {
 
   return (
     <div className="w-full px-12 mt-4">
-      <h1>"Pixel-Perfect, Hand-Crafted Websites
+      <h1 className="">"Pixel-Perfect, Hand-Crafted Websites
       Built with Clean Code"</h1>
 
-      <div className="relative w-full h-[600px] mt-8">
+      <div className="relative w-full h-[400px] mt-8 ">
         <img
           src="/public/S1.svg"
           alt="Background"
-          className="absolute right-0 top-0 h-full object-contain w-[60%]"
+          className="absolute right-0 top-0 h-full object-contain w-[55%]"
         />
-        <div className="absolute inset-0 flex flex-col items-end justify-center text-black text-right px-4 transform -translate-x-76 transform -translate-y-26">
-          <h2 className="text-3xl font-bold">We Build Beautiful Websites</h2>
-          <p className="mt-2 text-lg">Custom coded to perfectly match <br/>your brand identity and business objectives.<br/> Every line of code is carefully<br/> crafted for optimal performance and user experience.</p>
+        <div className="absolute right-0 top-2/5 transform -translate-y-1/2 transform -translate-x-46 w-[25%] border bg-gray-900 rounded-xl opacity-75 text-white p-4 flex flex-col items-end justify-center text-black text-left border px-4">
+
+          <h2 className="text-2xl font-bold">We Build Beautiful Websites</h2>
+          <p className="mt-2 ">Custom coded to perfectly match <br/>your brand identity and business objectives.<br/> Every line of code is carefully<br/> crafted for optimal performance<br/> and user experience.</p>
         </div>
         
       
           <img src="S2.svg"
            alt="Background"
-           className="absolute left-50 top-0 h-full object-contain w-[30%] mt-56"/>
-          <div className="absolute inset-0 flex flex-col items-start justify-center text-black text-left px-4 transform translate-x-55 transform translate-y-56">
-          <h2 className="text-3xl font-bold">Premium Web Development <br/>Services</h2>
-          <p className="mt-2 text-lg">From responsive design to e-commerce solutions, <br/>we create digital experiences that<br/> engage visitors and convert them into customers.</p>
+           className="absolute left-35 top-0 h-full object-contain w-[30%] mt-20"/>
+          <div className="absolute flex flex-col items-start justify-center border bg-gray-900 rounded-xl opacity-75 text-white p-4 text-left px-4 transform translate-x-65 transform translate-y-60">
+          <h2 className="text-2xl font-bold text-white p-2">Premium Web Development <br/>Services</h2>
+          <p className="mt-2 ">From responsive design to e-commerce solutions, <br/>we create digital experiences that engage visitors<br/> and convert them into customers.</p>
     
         </div>
       </div>
@@ -43,7 +44,7 @@ function Hero() {
         Contact us today to discuss your project and see how we can help elevate your brand with a custom web solution.
         </h3>
         <Link to="/Contact">
-          <button className="border p-2 bg-sky-800">Get Started</button>
+          <button className="border rounded-lg hover:bg-blue-100 p-2 bg-sky-800">Get Started</button>
         </Link>
       </div>
 
