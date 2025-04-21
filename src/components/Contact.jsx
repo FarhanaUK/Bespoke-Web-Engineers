48f5d32340f3b3f8bf94d750f4ceb0fedd00bcc1
Farhana Aktar <farhanaaktar@live.co.uk>
2025-04-21 17:56:49 +0100

    label added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,53 @@
 import { useState } from "react"
 
 
+
+
 function Contact () {
 const [value, setValue] = useState("")
+
+const onChange = () => {
+setValue = {value, }
+
+}
+
+
+
+const onSbumit = (evt) =>{
+    evt.preventDefault()
+    }
+
 
     return (
 
         <div className="flex flex-col justify-center items-center space-y-4"> 
             <h1 className="font-semibold text-4xl ">Contact Us</h1>
-            <p>Got a question? Drop a message below!</p>
-            <div className="w-1/2 flex flex-col mb-4">
+            <p className="text-gray-700">Got a question? Drop a message below!</p>
+            <div className="mb-4">
 
-                <lable>Name</lable>
-                <input type="text" name="name" value="" className="border mb-6"/>
-                <lable>Email</lable>
-                <input type="email" name="email" value="" className="border mb-6"/>
-                <lable>Message</lable>
-                <input type="text" name="message" value="" className="border mb-6 "/>
-                <div>
+            <form onSubmit={onSbumit} className="flex flex-col">
+                <label className="font-semibold">Name
+                <input type="text" name="name" value="" onChange="" className="border mb-6 flex flex-col w-full"/>
+                </label>
+
+                <label className="font-semibold">Email
+                <input type="email" name="email" value="" onChange=""  className="border mb-6 flex flex-col w-full"/>
+                </label>
+
+                <label className="font-semibold">Message
+                <input type="text" name="message" value="" onChange="" className="border mb-6 flex flex-col w-full "/>
+                </label>
+ <div>
             <button className="border p-2 rounded-lg bg-black text-white">Send Message</button>
 
+</div>
+</form>
+
+
+               
+
             <p className="py-2 text-gray-500">or email directly for enquires at <span className="text-blue-500">enquires@bespokewebengineers.com</span></p>
-            </div>
+            
             </div>
         </div>
     )
